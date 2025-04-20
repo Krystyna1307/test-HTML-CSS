684132163221940a2330cedfa0c28de8221c2c03
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-04-20 16:45:50 +0200

    btn hero

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,22 +2,35 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  const menuLinks = document.querySelectorAll(".mob-menu-link");
+
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
+
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
       : "enableBodyScroll";
+
     bodyScrollLock[scrollLockMethod](document.body);
   };
+
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  // Close the mobile menu on wider screens if the device orientation changes
+  // Закриваємо меню при кліку на будь-який пункт меню
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.remove("is-open");
+      openMenuBtn.setAttribute("aria-expanded", false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  });
+
+  // Закриваємо меню при зміні ширини вікна на >=768px
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
